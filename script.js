d3b0c8db78c02712e8ed6340b12efe9513b53c0a
Aaron Abele <aaronabele.coding@gmail.com>
2023-01-10 22:24:01 +0100

    adds solution

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 let counter = 0;
-const btn = document.querySelector("#zaehler");
+const mainBtn = document.querySelector("main");
+const mainCounter = document.querySelector("#zaehler");
 const btnReset = document.querySelector("#reset");
 
-btn.addEventListener("click", function () {
-  btn.innerHTML = counter++;
+//Increase Counter
+function increaseCounter() {
+  mainCounter.innerHTML = counter++;
+  if (counter === 101) {
+    counter = 1;
+  }
+  mainBtn.style.setProperty("--counter", counter + "%");
+}
+
+mainBtn.addEventListener("click", increaseCounter);
+
+//Reset Counter
+function resetCounter() {
+  counter = 0;
+  mainCounter.innerHTML = 0;
+  mainBtn.style.setProperty("--counter", 0 + "%");
+}
+
+btnReset.addEventListener("click", resetCounter);
+
+//stop Propagation
+btnReset.addEventListener("click", function (e) {
+  e.stopPropagation();
+  resetCounter();
 });
 
-btnReset.addEventListener("click", function () {
-  counter = 0;
-  btn.innerHTML = 0;
+//Enble Space and Enter for Counting
+document.addEventListener("keypress", function (e) {
+  if (e.code === "Space" || e.code === "Enter") {
+    increaseCounter();
+    resetCounter;
+  }
 });
